48bfede5cdbac417b49a65038f2979319d58e3bb
Penelope Maher <p.maher@exeter.ac.uk>
2017-09-18 17:27:31 +0100

    Bug fix to change to branch. Also added the no edit option to the merge so it does not prompt for a merge message.

--- update_version.sh
+++ update_version.sh
@@ -14,10 +14,13 @@ if [[ ${#var[@]} -eq 0 ]]; then
   echo 'Performing merge of master to all author branches'
   for author in "${author_list[@]}"
   do
-
-    git merge -X theirs master
+    git checkout $branch_prefix$author
+    git merge -X theirs master --no-edit
   done
 else
   echo 'ERROR: Unable to merge master to author branches'
   echo 'master branch is different from $editing_branch branch'
 fi
+
+git checkout master
+echo 'Finished updating revision. You are back on the master.'
